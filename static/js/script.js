e49fb01e379ad20cc538cfa767e2a42da9f18acf
Alwin M Reji <01fe17bar005@kletech.ac.in>
2022-05-05 11:20:03 +0530

    first repo

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+is_Signed_In = true
+window.onload = isSignedIn;
+
+function isSignedIn() {
+    if (is_Signed_In == true) {
+        // getDetails();
+        document.getElementById('SignUp-Here').style.display = 'none';
+    }
+    else {
+        document.getElementById('Account-Menu').style.display = 'none';
+    }
+}
---
+++ store/models.py
@@ -0,0 +1,99 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Address(models.Model):
+    city=models.CharField(max_length=20)
+    streetaddress=models.TextField()
+    state=models.CharField(max_length=30)
+    country=models.CharField(max_length=30)
+    Zipcode=models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.id)
+
+
+ACCOUNT_STATUS=(
+    ('Active','Active'),
+    ('Blocked','Blocked'),
+    ('Banned','Banned'),
+    ('Archived','Archived'),
+    ('Unkown','Unkown'),
+    ('Compromised','Compromised'),
+)
+
+class Account(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    status=models.CharField(max_length=50,choices=ACCOUNT_STATUS,default="Active")
+    email=models.EmailField(max_length=250)
+    phone=models.CharField(max_length=10)
+    address=models.ForeignKey(Address,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCategory(models.Model):
+    name=models.CharField(max_length=250)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name=models.CharField(max_length=200)
+    description=models.TextField()
+    price=models.IntegerField
+    available_count=models.IntegerField()
+    category=models.ManyToManyRel(ProductCategory)
+
+    def __str__(self):
+        return self.name
+
+
+
+ORDER_STATUS=(
+    ('Unshipped','Unshipped'),
+    ('Pending','Pending'),
+    ('Cancelled','Cancelled'),
+    ('RefundApplicable','RefundApplicable'),
+    ('Complete','Complete'),
+    ('Shipped','Shipped'),
+)
+
+class Order(models.Model):
+    orderNo=models.IntegerField()
+    status=models.CharField(max_length=50,choices=ORDER_STATUS,default="Unshipped")
+    orderDate=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.orderNo)
+
+PAYMENT_STATUS=(
+    ('Failed','Failed'),
+    ('Unpaid','Unpaid'),
+    ('Pending','Pending'),
+    ('Completed','Completed'),
+    ('Declined','Declined'),
+    ('Settling','Settling'),
+    ('Settled','Settled'),
+    ('Cancelled','Cancelled'),
+    ('Abandoned','Abandoned'),
+    ('Refunded','Refunded'),
+)
+
+class Payment(models.Model):
+    status=models.CharField(max_length=50,choices=PAYMENT_STATUS,default="Unshipped")
+    amount=models.FloatField()
+    order=models.ForeignKey(Order,on_delete=models.CASCADE,unique=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+
+    
+
+
