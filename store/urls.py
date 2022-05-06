6cec32f112177d2236910c40cf960531e5f2511d
Alwin M Reji <01fe17bar005@kletech.ac.in>
2022-05-06 20:20:24 +0530

    siginpage

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('sign-up/', views.signup, name='sigin-up'),
+    path('login/', views.signin, name='login'),
+
+
 ]
